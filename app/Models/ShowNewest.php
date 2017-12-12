244653726a04095147749dea39c72e1f72a2e34c
Louvian Lyndal <louvianlyndal@gmail.com>
2017-12-12 22:22:48 +0700

    Update limit

--- app/Models/ShowNewest.php
+++ app/Models/ShowNewest.php
@@ -6,9 +6,9 @@ use System\DB;
 
 class ShowNewest
 {
-	public static function get()
+	public static function get($limit)
 	{
-		$st = DB::prepare("SELECT * FROM `pururin_main_data` ORDER BY `created_at` DESC LIMIT 10;");
+		$st = DB::prepare("SELECT * FROM `pururin_main_data` ORDER BY `created_at` DESC LIMIT {$limit};");
 		$st->execute();
 		return $st->fetchAll();
 	}
