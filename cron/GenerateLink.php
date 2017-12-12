f8a55669933848981d79c2f72060268c1a282312
Louvian Lyndal <louvianlyndal@gmail.com>
2017-12-13 00:42:13 +0700

    Update link generate

--- cron/GenerateLink.php
+++ cron/GenerateLink.php
@@ -26,7 +26,7 @@ class GenerateLink
 		} else {
 			$pointer = 1;
 		}
-		$ch = new Curl("http://pururin.us/browse/search?q=rape&sType=normal&page=".$pointer);
+		$ch = new Curl("http://pururin.us/browse/search?q=full+color&sType=normal&page=".$pointer);
 		$a = explode("<div class=\"gallery-listing\"", $ch->exec(), 2);
 		if (! isset($a[1])) {
 			return [];
