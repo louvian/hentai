a8a63f4e5ebc7cd7de725feb602fff2a02e77824
Louvian Lyndal <louvianlyndal@gmail.com>
2017-12-12 22:21:18 +0700

    Increase limit

--- app/Views/index.php
+++ app/Views/index.php
@@ -34,7 +34,7 @@
 				document.getElementById('bound').innerHTML = buildData(JSON.parse(this.responseText));
 			}
 		};
-		ch.open("GET", "https://hentai.mieinstance.cf/api.php?page=newest");
+		ch.open("GET", "https://hentai.mieinstance.cf/api.php?page=newest&limit=27");
 		ch.send(null);
 </script>
 </body>
