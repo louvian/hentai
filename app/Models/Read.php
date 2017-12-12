8578ab301b0b692ba79b4da4dfa96543ee9ba69f
Louvian Lyndal <louvianlyndal@gmail.com>
2017-12-12 22:16:05 +0700

    Create reader

---
+++ app/Models/Read.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Models;
+
+use PDO;
+use System\DB;
+
+class Read
+{
+	public static function get($id)
+	{
+		$st = DB::prepare("SELECT * FROM `pururin_main_data` WHERE `id`=:id LIMIT 1;");
+		$st->execute([':id' => $id]);
+		return $st->fetch(PDO::FETCH_ASSOC);
+	}
+}
--- app/Reader.php
+++ app/Reader.php
@@ -7,4 +7,19 @@ class Reader
 	public function __construct()
 	{
 	}
+
+	public function run()
+	{
+		if (! isset($_GET['id'])) {
+			http_response_code(400);
+			exit("parameter required");
+		}
+		$dt = \App\Models\Read::get($_GET['id']);
+		if (! $dt) {
+			http_response_code(404);
+			exit("Not found");
+		}
+		$dt['info'] = json_decode($dt['info'], true);
+		return view("read", ["dt" => $dt]);
+	}
 }
