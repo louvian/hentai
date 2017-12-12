686c5709831c85701a45aec110d599443c06eda6
Louvian Lyndal <louvianlyndal@gmail.com>
2017-12-12 19:00:22 +0700

    Fix scraper

--- cron/GenerateLink.php
+++ cron/GenerateLink.php
@@ -19,6 +19,18 @@ class GenerateLink
 
 	private static function pururin()
 	{
-		
+		$a = file_get_contents("../a.tmp");
+		$a = explode("<div class=\"gallery-listing\"", $a, 2);
+		$a = explode("<a href=\"", $a[1]);
+		$link = [];
+		foreach ($a as $val) {
+			$val = explode("\"", $val, 2);
+			if (substr($val[0], 0, 26) === "http://pururin.us/gallery/") {
+				$val[0] = explode("/", $val[0]);
+				unset($val[0][5]);
+				$link[] = implode("/", $val[0]);
+			}
+		}
+		return $link;
 	}
 }
--- cron/pururin.php
+++ cron/pururin.php
@@ -10,36 +10,39 @@ if (file_exists(__DIR__."/assets/pururin/lock")) {
 
 
 require __DIR__ . "/../vendor/autoload.php";
-http://pururin.us/browse/search?q=rape&sType=normal&page=1
+
 $saveDir  = __DIR__ . "/../assets/pururin";
-$mangaUrl = "http://pururin.us/gallery/35052/";
+$mangaUrls = Cron\GenerateLink::generate('pururin');
 
-if (! is_dir($saveDir)) {
-	mkdir($saveDir);
-}
+foreach ($mangaUrls as $mangaUrl) {
+	if (! is_dir($saveDir)) {
+		mkdir($saveDir);
+	}
 
-$app = new Pururin\PururinCrawler(
-	[
-		"save_directory" => $saveDir,
-		"manga_url"		 => $mangaUrl
-	]
-);
+	$app = new Pururin\PururinCrawler(
+		[
+			"save_directory" => $saveDir,
+			"manga_url"		 => $mangaUrl
+		]
+	);
 
-if ($app->run()) {
-	$st = \System\DB::prepare("INSERT INTO `pururin` (`id`, `title`, `info`, `origin_link`, `created_at`, `updated_at`) VALUES (:id, :title, :info, :origin_link, :created_at, :updated_at);");
-	$st->execute(array_merge($data = $app->getResult(), ["created_at" => date("Y-m-d H:i:s"), "updated_at" => null]));
-	$data['info'] = json_decode($data['info']);
-	if (isset($data['info']['Content'])) {
-		$query = "INSERT INTO `pururin_genres` (`id`,`genre`) VALUES ";
-		$i = 0;
-		$queryValue = [
-			":id" => $data['id']
-		];
-		foreach ($data['Content'] as $val) {
-			$query .= "(:id, :genre{$i}),";
-			$data[':genre'.$i] = $val;
+	if ($app->run()) {
+		$st = \System\DB::prepare("INSERT INTO `pururin_main_data` (`id`, `title`, `info`, `origin_link`, `created_at`, `updated_at`) VALUES (:id, :title, :info, :origin_link, :created_at, :updated_at);");
+		$st->execute(array_merge($data = $app->getResult(), ["created_at" => date("Y-m-d H:i:s"), "updated_at" => null]));
+		$data['info'] = json_decode($data['info'], true);
+		if (isset($data['info']['Contents'])) {
+			$query = "INSERT INTO `pururin_genres` (`id`,`genre`) VALUES ";
+			$i = 0;
+			$queryValue = [
+				":id" => $data['id']
+			];
+			foreach ($data['info']['Contents'] as $val) {
+				$query .= "(:id, :genre{$i}),";
+				$queryValue[':genre'.($i++)] = $val;
+			}
+			$st = \System\DB::prepare(rtrim($query, ","));
+			$exe = $st->execute($queryValue);
+			var_dump($st->errorInfo());
 		}
-		$st = DB::prepare(rtrim($query, ","));
-		$st->execute($queryValue);
 	}
 }
